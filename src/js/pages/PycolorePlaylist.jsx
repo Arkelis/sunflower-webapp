ec228f46cda0fd3d6bb835056c6e6896d2b88fb2
Adrien Tiburce <adrien.tiburce@gmail.com>
2020-10-16 12:44:03 +0200

    Feat: search a song by title or album in Playlist Pycolore

--- src/js/pages/PycolorePlaylist.jsx
+++ src/js/pages/PycolorePlaylist.jsx
@@ -4,23 +4,42 @@ import 'regenerator-runtime/runtime'
 export default function PycolorePlaylist({apiHost}) {
 
     const [playlist, setPlaylist] = useState([])
+    const [filteredPlaylist, setFilteredPlaylist] = useState([])
 
     useEffect(() => {
         async function fetchPlaylist() {
             const resp = await fetch(`${apiHost}/stations/pycolore/playlist/?shape=groupartist`)
             const json = await resp.json()
             setPlaylist(json)
+            setFilteredPlaylist(json)
         }
         fetchPlaylist()
     }, [])
 
-    const formattedPlaylist = formatPlaylist(playlist)
+
+    const filterPlaylist = event => {
+        let input = event.target.value.toLowerCase();
+        let copyPlaylist = Object.assign({}, playlist)
+        Object.keys(copyPlaylist).forEach(key => {
+            copyPlaylist[key] = filterArtist(copyPlaylist[key], input)
+        });
+        setFilteredPlaylist(copyPlaylist);
+    }
+
+    const filterArtist = (artistSongs, input) => {
+        return artistSongs.filter(song => {
+            return song.album.toLowerCase().includes(input) || song.title.toLowerCase().includes(input)
+        })
+    }
+
+    const formattedPlaylist = formatPlaylist(filteredPlaylist)
 
     function formatPlaylist(playlist) {
         const groups = []
         for (let artist in playlist) {
             const songs = playlist[artist]
             const size = songs.length
+            if (size == 0) continue;
             const songsRows = []
             for (let i = 1; i < size; i++) {
                 songsRows.push(
@@ -32,10 +51,6 @@ export default function PycolorePlaylist({apiHost}) {
             }
             groups.push(
                 <>
-                    <tr className="artist-row">
-                        <td></td>
-                        <td colSpan='2' className='artist-cell'>{artist}</td>
-                    </tr>
                     <tr>
                         <th className="artist-th" rowSpan={size} scope="rowgroup">{artist}</th>
                         <td>{songs[0].title}</td>
@@ -50,6 +65,8 @@ export default function PycolorePlaylist({apiHost}) {
 
     return <>
         <h2>Playlist Pycolore</h2>
+
+        <input type="text" placeholder="Rechercher un titre ou un album..." onChange={filterPlaylist}/>
         <div id="playlist-wrapper">
             <table>
                 <colgroup>
